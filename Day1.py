8bbbdfac406a9a156fb0ab2eebd2871055c45773
sartajbalti <sartajbalti@gmail.com>
2024-01-01 18:29:44 +0900

    updated

--- Day1.py
+++ Day1.py
@@ -1,2 +1,37 @@
-print("My Name is Sartaj")
-print("My first name is Sartaj\nSecond name is Ahmed")
+# # print("My Name is Sartaj")
+# # print("My first name is Sartaj\nSecond name is Ahmed")
+
+# # input a prompt for the user to get the data
+# name = input("what is your name? ")
+# print("Hello: ", name)
+
+# # variables are used to store some information to use at any point in the code / like almost save the data
+# n = "jack"
+# print(n) 
+# length = len(n)
+# print(length)
+# # switch two values of that stored in the variables 
+# a = input()
+# b = input()
+# # switch
+# c = a #c as a temp value for storing the value of a 
+# a = b # assign vale of b to a 
+# b = c # now the value stored in c which is a value of a assign to b 
+# print(a,b)
+
+# # naming a variable you can name what ever you want just ensure to make your code to be readable that make sense to you. Code readability
+
+# Band name Generator 
+# 1. Creat a Greetings
+print("Welcome to the Brand name Generator....")
+
+# 2. Ask the user for the city they grewup 
+city = input("Which city did you grewup ?")
+
+#  3. Ask the user their pet name
+pet = input("What is your pet name ?")
+
+#  4. combine the city name and pet name to make a Brand name
+
+brand = city + pet 
+print("The brand name we suggested is ", brand)
