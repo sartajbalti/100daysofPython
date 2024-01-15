e3c8da0ee716d72ad0cad7187a39de72a3da5116
sartajbalti <sartajbalti@gmail.com>
2024-01-15 20:41:59 +0900

    Day 6 added

---
+++ Day6.py
@@ -0,0 +1,35 @@
+# code blocks
+# functions
+
+# def function_name():
+#     # code block
+# built in functions
+# print()
+# input()
+# len()
+# type()
+# int()
+# float()
+# str()
+# bool()
+# list()
+# dict()
+# set()
+# tuple()
+# range()
+
+# define the function and write the codes to run when the function is called
+
+# def my_function():
+#     print("Hello from a function")
+# my_function()
+
+# def my_function(fname):
+#     print(fname + " Refsnes")
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+def my_function(fname, lname):
+    print(fname + " " + lname)
+my_function("Emil", "Refsnes")
